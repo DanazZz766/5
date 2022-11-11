66e558111ab8e49882b3f73f93df4d290e0752a2
DanazZz766 <111424866+DanazZz766@users.noreply.github.com>
2022-11-11 15:51:26 +0800

    Add files via upload

---
+++ dzlx.js
@@ -0,0 +1,15 @@
+var dzcs = document.getElementById("dzs").innerHTML;
+function func_dz() {
+  dzcs++;
+  document.getElementById("dzs").innerHTML = dzcs;
+}
+var dzcs = document.getElementById("dzs1").innerHTML;
+function func_dz1() {
+  dzcs++;
+  document.getElementById("dzs1").innerHTML = dzcs;
+}
+var dzcs = document.getElementById("dzs2").innerHTML;
+function func_dz2() {
+  dzcs++;
+  document.getElementById("dzs2").innerHTML = dzcs;
+}
